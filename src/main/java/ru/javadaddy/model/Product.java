9fc2e05bbe123d27780faa7bacf726e61ca4046f
T1mono <167001770+T1mono@users.noreply.github.com>
2025-06-08 20:22:00 +0500

    Added Product

---
+++ src/main/java/ru/javadaddy/model/Product.java
@@ -0,0 +1,51 @@
+package ru.javadaddy.model;
+
+import java.util.Objects;
+
+public class Product {
+    private String name;
+
+    private String price;
+
+    public Product(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
